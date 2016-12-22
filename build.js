7648981f0b74263dffe058b3ef9ed98982ce8696
Francesc Rosàs <francescrosasbosque@gmail.com>
2016-12-22 10:53:06 +0000

    Fix .DS_Store being included to the list of images

--- build.js
+++ build.js
@@ -1,16 +1,20 @@
 const ejs = require('ejs');
 const fs = require('fs');
 const {promisify} = require('potpourri/dist/es5');
+const path = require('path');
 
 process.on('unhandledRejection', error => { throw error; });
 
 const whenTemplate = promisify(fs.readFile)('index.ejs', 'utf8');
 
 const imagesDir = 'images';
-const whenImages = promisify(fs.readdir)(`public/${imagesDir}`).
-    then(images => images.map(image => `${imagesDir}/${image}`));
+const whenImages = promisify(fs.readdir)(`public/${imagesDir}`).then(file => {
+    return file.filter(file => isImage(file)).map(file => `${imagesDir}/${file}`);
+});
     
 Promise.all([whenTemplate, whenImages]).then(([template, images]) => {
     const html = ejs.render(template, {images});
     return promisify(fs.writeFile)('public/index.html', html);
 });
+
+const isImage = file => ['.gif', '.jpg', '.png'].includes(path.extname(file));
